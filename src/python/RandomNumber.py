521e2f3e2828a3a4a910427565e9841abea0d506
Knight.zhou <YoungWelle@gmail.com>
2012-10-09 19:30:59 +0800

    ADDED:red_blue_balls.csv file;

--- src/python/RandomNumber.py
+++ src/python/RandomNumber.py
@@ -7,7 +7,7 @@ redBalls=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,2
 blueBalls=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 #test code
 #print( random.random() )
-for x in range(0,7):#NUM_OF_RED=6
+for x in range(0,6):#NUM_OF_RED=6
     choice_num_red = random.choice( redBalls )
     print( choice_num_red )
     redBalls.remove(choice_num_red)
@@ -15,7 +15,7 @@ for y in range(0,1):#NUM_OF_BLUE=1
     choice_num_blue = random.choice( blueBalls )
     print( choice_num_blue )
 #scipy test code
-a = zeros(100)
+a = zeros(1000)
 a[:100] = 1
 print(a)
 b = fft(a)
@@ -23,3 +23,11 @@ print(b)
 #matplotlib test 
 print(pylab.plot(abs(b)))
 #show()
+#from matplotlib.mlab import normpdf
+#import matplotlib.numerix as nx
+#import pylab as p
+#
+#x = nx.arange(-4, 4, 0.01)
+#y = normpdf(x, 0, 1) # unit normal
+#p.plot(x,y, color='red', lw=2)
+#p.show()
