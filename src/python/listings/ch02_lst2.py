3bf00616a550352db4939b9e3d9cdcbebed469bd
Knight.zhou <youngwelle@gmail.com>
2013-01-07 23:44:18 +0800

    fixed the invalid data at ch02_presidents.txt

--- src/python/listings/ch02_lst2.py
+++ src/python/listings/ch02_lst2.py
@@ -8,6 +8,23 @@ def kde( z, w, xv ):
 d = loadtxt( "../datasets/ch02_presidents.txt", usecols=(2,) )
 
 w = 2.5
-
+alist = []
 for x in linspace( min(d)-w, max(d)+w, 1000 ):
-    print x, kde( x, w, d )
+    print x, kde( x, w, d );alist.append(kde( x, w, d ))
+print alist
+
+import matplotlib.pyplot as plt
+data = [1.5]*7 + [2.5]*2 + [3.5]*8 + [4.5]*3 + [5.5]*1 + [6.5]*8
+plt.hist(data, bins=6)
+plt.show()
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
+data = [1.5]*7 + [2.5]*2 + [3.5]*8 + [4.5]*3 + [5.5]*1 + [6.5]*8
+density = gaussian_kde(data)
+xs = np.linspace(0,8,200)
+density.covariance_factor = lambda : .25
+density._compute_covariance()
+plt.plot(xs,density(xs))
+plt.show()
